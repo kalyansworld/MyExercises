2820ca19b8462d196bb1edfd3fb7b670809578ff
kalyan <kalyan@kalyan-PC>
2016-09-04 16:27:04 +0530

    Added Group entity

---
+++ ExpenseCalculator/src/main/java/com/kalyan/entity/Group.java
@@ -0,0 +1,54 @@
+package com.kalyan.entity;
+
+import java.util.List;
+
+public class Group {
+	
+	private String name;
+	private List<User> userList;
+	private List<Expense> expenseList;
+	private String owner;
+	private String creationDate;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<User> getUserList() {
+		return userList;
+	}
+	
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
+	
+	public List<Expense> getExpenseList() {
+		return expenseList;
+	}
+	
+	public void setExpenseList(List<Expense> expenseList) {
+		this.expenseList = expenseList;
+	}
+	
+	public String getOwner() {
+		return owner;
+	}
+	
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+	
+	public String getCreationDate() {
+		return creationDate;
+	}
+	
+	public void setCreationDate(String creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+
+}
